aa87df23715406de0e31879b9e04ff9c09ade5a2
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-07-04 03:38:14 +0300

    Add files via upload

---
+++ xtraevent-plugin_v6.6.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+#
+cd /tmp
+set -e 
+ wget "https://raw.githubusercontent.com/Ham-ahmed/xtra/main/xtraevent-6.6.tar.gz"
+wait
+tar -xzf xklass-v016.tar.gz  -C /
+wait
+cd ..
+set +e
+rm -f /tmp/xklass-v016.tar.gz
+sleep 2;
+echo "" 
+echo "" 
+echo "*********************************************************"
+echo "#                   INSTALLED SUCCESSFULLY              #"
+echo "*                       ON - Panel                      *"
+echo "*                Enigma2 restart is required            *"
+echo "*********************************************************"
+echo "               UPLOADED BY  >>>>   HAMDY_AHMED           "
+sleep 4;
+	echo '================================================='
+###########################################                                                                                                                  
+echo ". >>>>         RESTARING     <<<<"
+echo "*********************************************************"
+wait
+killall -9 enigma2
+exit 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
